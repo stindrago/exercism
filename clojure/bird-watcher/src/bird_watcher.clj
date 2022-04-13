a4eb5db8097749161db9ab2e6de6ae3e6179f308
Costin Dragoi <costin@dragoi.me>
2022-04-13 11:28:26 +0200

    update exercise

--- clojure/bird-watcher/src/bird_watcher.clj
+++ clojure/bird-watcher/src/bird_watcher.clj
@@ -1,22 +1,25 @@
 (ns bird-watcher)
 
-(def last-week 
-  )
+(def last-week
+  [0 2 5 3 7 8 4])
 
 (defn today [birds]
-  )
+  (last birds))
 
 (defn inc-bird [birds]
-  )
+  (assoc birds 6 (inc (today birds))))
 
 (defn day-without-birds? [birds]
-  )
+  (contains? birds 0))
 
-(defn n-days-count [birds n]
-  )
+(defn n-days-count [birds n])
 
-(defn busy-days [birds]
-  )
+(defn busy-days [birds])
 
-(defn odd-week? [birds]
-  )
+(defn odd-week? [birds])
+
+;; In-line CIDER evaluation with C-c C-e. Info @ https://docs.cider.mx/cider/usage/code_evaluation.html
+(comment
+  last-week
+  (today last-week)
+  (inc-bird last-week))
