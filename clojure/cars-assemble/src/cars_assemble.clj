e01496307bbf0b57dc2ae1d0ea0b445e61c82258
Costin Dragoi <costin@dragoi.me>
2022-04-13 17:39:23 +0200

    Update exercise to complete

--- clojure/cars-assemble/src/cars_assemble.clj
+++ clojure/cars-assemble/src/cars_assemble.clj
@@ -4,20 +4,16 @@
   "Returns the assembly line's production rate per hour,
    taking into account its success rate"
   [speed]
-  (let [cars 221]
-    (case speed
-      0 (* cars speed 0)
-      1 (* cars speed 1)
-      2 (* cars speed 1)
-      3 (* cars speed 1)
-      4 (* cars speed 1)
-      5 (* cars speed 0.9)
-      6 (* cars speed 0.9)
-      7 (* cars speed 0.9)
-      8 (* cars speed 0.9)
-      9 (* cars speed 0.8)
-      10 (* cars speed 0.77))))
+  (let [cars-ph 221                     ; cars per hour
+        speed-rates (conj {0 0/100}
+                          (zipmap (range 1 5) (repeat 100/100))
+                          (zipmap (range 5 9) (repeat 90/100))
+                          {8 90/100}
+                          {9 80/100}
+                          {10 77/100})] ; => {0 0, 7 9/10, 1 1, 4 1, 6 9/10, 3 1, 2 1, 9 4/5, 5 9/10, 10 77/100, 8 9/10}
+    (* cars-ph speed (double (speed-rates speed))))) ; `double` instead of `float`
 
 (defn working-items
   "Calculates how many working cars are produced per minute"
-  [speed])
+  [speed]
+  (int (/ (production-rate speed) 60)))
