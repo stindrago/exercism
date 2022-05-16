594ff83c9bfdd59086b8442d0a4244224eb46f47
Costin Dragoi <costin@dragoi.me>
2022-05-16 12:17:00 +0200

    update exercise

--- clojure/elyses-destructured-enchantments/src/elyses_destructured_enchantments.clj
+++ clojure/elyses-destructured-enchantments/src/elyses_destructured_enchantments.clj
@@ -3,23 +3,23 @@
 (defn first-card
   "Returns the first card from deck."
   [deck]
-)
+  (first deck))
 
 (defn second-card
   "Returns the second card from deck."
   [deck]
-)
+  (second deck))
 
 (defn swap-top-two-cards
   "Returns the deck with first two items reversed."
   [deck]
-)
+  (vec (conj (drop 2 deck) (first-card deck) (second-card deck))))
 
 (defn discard-top-card
   "Returns a vector containing the first card and
    a vector of the remaining cards in the deck."
   [deck]
-)
+  (conj [] (first deck) (vec (rest deck))))
 
 (def face-cards
   ["jack" "queen" "king"])
@@ -27,4 +27,4 @@
 (defn insert-face-cards
   "Returns the deck with face cards between its head and tail."
   [deck]
-)
+  (vec (flatten (conj (rest deck) face-cards (first deck)))))
