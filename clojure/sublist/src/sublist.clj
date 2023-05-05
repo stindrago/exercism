0d835e733dfaccf97416757434ba60e8810d35b7
Costin Dragoi <costin@dragoi.me>
2023-05-05 15:04:46 +0200

    Completed

--- clojure/sublist/src/sublist.clj
+++ clojure/sublist/src/sublist.clj
@@ -1,5 +1,20 @@
 (ns sublist)
 
-(defn classify [list1 list2] ;; <- arglist goes here
-      ;; your code goes here
-)
+(defn is-sublist? [list1 list2]
+  (or (empty? list1)
+      (some #(= list1 (subvec (vec %) 0 (count list1))) (partition (count list1) 1 list2))))
+
+(defn classify [list1 list2]
+  (cond
+    (= list1 list2) :equal
+    (is-sublist? list1 list2) :sublist
+    (is-sublist? list2 list1) :superlist
+    :else :unequal))
+
+(comment
+  (classify [1 2 3] [1 2 3 4 5]) ; Returns :sublist
+  (classify [3 4 5] [1 2 3 4 5]) ; Returns :sublist
+  (classify [3 4] [1 2 3 4 5])   ; Returns :sublist
+  (classify [1 2 3] [1 2 3])     ; Returns :equal
+  (classify [1 2 3 4 5] [2 3 4]) ; Returns :superlist
+  (classify [1 2 4] [1 2 3 4 5])) ; Returns :unequal
